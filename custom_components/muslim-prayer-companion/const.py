223a936fd4f9e8963e1e496da149f9b3e03f8ca5
abdelrahman maharek <abdelrahman.maharek@2k.com>
2025-03-13 09:44:23 +0000

    Update constants in Muslim Prayer Companion component for clarity and functionality

--- custom_components/muslim-prayer-companion/const.py
+++ custom_components/muslim-prayer-companion/const.py
@@ -1,15 +1,26 @@
 """
-Constants for the Muslim Prayer component.
+Constants for the Muslim Prayer Companion component.
 """
 
 from typing import Final
-from logging import Logger, getLogger
+from logging import getLogger
 
-DOMAIN: Final = "muslim-prayer-companion"
+DOMAIN: Final = "muslim_prayer_companion"
 NAME: Final = "Muslim Prayer Companion"
-PRAYER_TIMES_ICON = "mdi:calendar-clock"
+PRAYER_TIMES_ICON: Final = "mdi:calendar-clock"
 
 CONF_CALC_METHOD: Final = "calculation_method"
+CONF_USE_API: Final = "use_api"  # Option to choose API-based prayer times.
+CONF_API_KEY: Final = "api_key"  # API key if required.
+CONF_IQAMAH_OFFSETS: Final = "iqamah_offsets"  # Offsets (in minutes) for Iqamah times.
+
+DEFAULT_IQAMAH_OFFSETS = {
+    "Fajr": 20,
+    "Dhuhr": 15,
+    "Asr": 15,
+    "Maghrib": 10,
+    "Isha": 15
+}
 
 CALC_METHODS = [
     "jafari",
@@ -28,10 +39,10 @@ CALC_METHODS = [
     "russia",
     "ie-icci",
     "ie-mcnd",
-    "ie-hicc",
+    "ie-hicc"
 ]
 
 DEFAULT_CALC_METHOD: Final = "ie-icci"
+DATA_UPDATED: Final = "muslim_prayer_data_updated"
 
-DATA_UPDATED = "Muslim_prayer_data_updated"
-LOGGER: Logger = getLogger(__package__)
+LOGGER = getLogger(__package__)
