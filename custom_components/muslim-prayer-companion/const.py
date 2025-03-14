eb1cb1273a0552cedc7d2f8a169daf48bddcc4a0
abdelrahman maharek <abdelrahman.maharek@2k.com>
2025-03-14 03:00:04 +0000

    Update calculation methods to use a dictionary for better readability and bump version to 2.2.0 in manifest.json

--- custom_components/muslim-prayer-companion/const.py
+++ custom_components/muslim-prayer-companion/const.py
@@ -23,25 +23,25 @@ DEFAULT_IQAMAH_OFFSETS = {
     "Isha": 15
 }
 
-CALC_METHODS = [
-    "jafari",
-    "karachi",
-    "isna",
-    "mwl",
-    "makkah",
-    "egypt",
-    "tehran",
-    "gulf",
-    "kuwait",
-    "qatar",
-    "singapore",
-    "france",
-    "turkey",
-    "russia",
-    "ie-icci",
-    "ie-mcnd",
-    "ie-hicc"
-]
+CALC_METHODS = {
+    "Jafari": "jafari",
+    "Karachi": "karachi",
+    "Islamic Society of North America (ISNA)": "isna",
+    "Muslim World League (MWL)": "mwl",
+    "Makkah": "makkah",
+    "Egypt": "egypt",
+    "Tehran": "tehran",
+    "Gulf": "gulf",
+    "Kuwait": "kuwait",
+    "Qatar": "qatar",
+    "Singapore": "singapore",
+    "France": "france",
+    "Turkey": "turkey",
+    "Russia": "russia",
+    "Ireland - Islamic Cultural Centre of Ireland (ICCI)": "ie-icci",
+    "Ireland - Muslim Community North Dublin (MCND)": "ie-mcnd",
+    "Ireland - Hansfield Islamic Cultural Centre (HICC)": "ie-hicc"
+}
 
 DEFAULT_CALC_METHOD: Final = "ie-icci"
 DATA_UPDATED: Final = "muslim_prayer_data_updated"
