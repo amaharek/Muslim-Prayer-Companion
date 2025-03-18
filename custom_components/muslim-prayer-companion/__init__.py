b1d39dfaf070203fc3765fe3b70f2e8064c688c8
abdelrahman maharek <abdelrahman.maharek@2k.com>
2025-03-18 12:31:29 +0000

    Refactor code for improved readability by formatting async_setup_entry and async_unload_entry functions

--- custom_components/muslim-prayer-companion/__init__.py
+++ custom_components/muslim-prayer-companion/__init__.py
@@ -28,7 +28,9 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, coordinator)
-    config_entry.async_on_unload(config_entry.add_update_listener(async_options_updated))
+    config_entry.async_on_unload(
+        config_entry.add_update_listener(async_options_updated)
+    )
     await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
 
     return True
@@ -36,7 +38,9 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Unload Muslim Prayer entry from config_entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS):
+    if unload_ok := await hass.config_entries.async_unload_platforms(
+        config_entry, PLATFORMS
+    ):
         coordinator: MuslimPrayerCompanionDataUpdateCoordinator = hass.data.pop(DOMAIN)
         if coordinator.event_unsub:
             coordinator.event_unsub()
